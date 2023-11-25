46254dd81dfbe79488ac65ddd586e8166c3861a7
DenisPaleha <portogas@gmail.com>
2023-11-25 21:42:53 +0100

    Spring, Bootstrap test project

---
+++ src/main/java/org/paleha/blog/controllers/MainController.java
@@ -0,0 +1,21 @@
+package org.paleha.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class MainController {
+    @GetMapping("/") // косая черта в кавычках означает что функция обрабатывает корневую или главную страницу
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница"); // Передаем параметры, которые будут применены к шаблону
+        return "home"; // возвращает шаблон домашней страницы
+    }
+
+    @GetMapping("/about") // под-страница about
+    public String about(Model model) {
+        model.addAttribute("title", "О нас"); // Передаем параметры, которые будут применены к шаблону
+        return "home"; // возвращает Пока все тот же шаблон домашней страницы
+    }
+
+
+}
