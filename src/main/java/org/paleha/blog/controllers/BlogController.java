2021015fac6c769c39d98c52d91449f53cb9908f
DenisPaleha <portogas@gmail.com>
2023-11-30 10:01:20 +0100

    Implementation of mySQL table (doesn't work!)

--- src/main/java/org/paleha/blog/controllers/BlogController.java
+++ src/main/java/org/paleha/blog/controllers/BlogController.java
@@ -1,5 +1,8 @@
 package org.paleha.blog.controllers;
 
+import org.paleha.blog.models.Post;
+//import org.paleha.blog.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller // Аннотация - контроллер
 public class BlogController {
 
+//    @Autowired
+//    private PostRepository postRepository;
+
     @GetMapping("/blog") // Аннотация определяет какую именно страницу отслеживает контроллер
     public String blogMine(Model model) { // Функция возвращает строку с названием шаблона, который мы должны подключить
+//        Iterable<Post> posts = postRepository.findAll();
+//        model.addAttribute("posts", posts); // Передаем данные по имени "posts" из объекта Iterable<Post>
         return "blog-main";
     }
 
---
+++ src/main/java/org/paleha/blog/repo/PostRepository.java
@@ -0,0 +1,7 @@
+//package org.paleha.blog.repo;
+//
+//import org.paleha.blog.models.Post;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface PostRepository extends CrudRepository<Post, Long> {
+//}
