27814fb70b991e61f95f6bf188673424fa560b11
DenisPaleha <portogas@gmail.com>
2023-12-21 17:11:40 +0100

    Mane class rewritten to work in a loop

--- src/main/java/org/paleha/blog/controllers/MainController.java
+++ src/main/java/org/paleha/blog/controllers/MainController.java
@@ -1,6 +1,5 @@
 package org.paleha.blog.controllers;
 
-import org.paleha.blog.Runner;
 import org.paleha.blog.main.Main;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
